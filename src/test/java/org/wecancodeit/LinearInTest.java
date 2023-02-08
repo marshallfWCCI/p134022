399d212b09a11bb7a9dfe7f1df88102d22d5f691
Marshall F <marshall@wecancodeit.org>
2023-02-08 08:32:58 -0500

    Some tests

---
+++ src/test/java/org/wecancodeit/LinearInTest.java
@@ -0,0 +1,122 @@
+package org.wecancodeit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class LinearInTest {
+    @Test
+    void test0_0() {
+        assertTrue(new LinearIn().linearIn(
+                new int[]{},
+                new int[]{}));
+    }
+
+    @Test
+    void test0_1() {
+        assertFalse(new LinearIn().linearIn(
+                new int[]{},
+                new int[]{1}));
+    }
+
+    @Test
+    void test1_0() {
+        assertTrue(new LinearIn().linearIn(
+                new int[]{1},
+                new int[]{}));
+    }
+
+    @Test
+    void test0_12() {
+        assertFalse(new LinearIn().linearIn(
+                new int[]{},
+                new int[]{1, 2}));
+    }
+
+    @Test
+    void test0_123() {
+        assertFalse(new LinearIn().linearIn(
+                new int[]{},
+                new int[]{1, 2, 3}));
+    }
+
+    @Test
+    void test1_1() {
+        assertTrue(new LinearIn().linearIn(
+                new int[]{1},
+                new int[]{1}));
+    }
+
+    @Test
+    void test12_12() {
+        assertTrue(new LinearIn().linearIn(
+                new int[]{1, 2},
+                new int[]{1, 2}));
+    }
+
+    @Test
+    void test12_13() {
+        assertFalse(new LinearIn().linearIn(
+                new int[]{1, 2},
+                new int[]{1, 3}));
+    }
+
+    @Test
+    void test012_13() {
+        assertFalse(new LinearIn().linearIn(
+                new int[]{0, 1, 2},
+                new int[]{1, 3}));
+    }
+
+    @Test
+    void test13_012() {
+        assertFalse(new LinearIn().linearIn(
+                new int[]{1, 3},
+                new int[]{0, 1, 2}));
+    }
+
+    @Test
+    void test129_13() {
+        assertFalse(new LinearIn().linearIn(
+                new int[]{1, 2, 9},
+                new int[]{1, 3}));
+    }
+
+    @Test
+    void test13_019() {
+        assertFalse(new LinearIn().linearIn(
+                new int[]{1, 3},
+                new int[]{0, 1, 9}));
+    }
+
+    @Test
+    void test0123456789_1357() {
+        assertTrue(new LinearIn().linearIn(
+                new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+                new int[]{1, 3, 5, 7}
+        ));
+    }
+
+    @Test
+    void test0123456789_13579() {
+        assertTrue(new LinearIn().linearIn(
+                new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+                new int[]{1, 3, 5, 7, 9}
+        ));
+    }
+
+    @Test
+    void test123456789_13579() {
+        assertTrue(new LinearIn().linearIn(
+                new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9},
+                new int[]{1, 3, 5, 7, 9}));
+    }
+
+    @Test
+    void test123456789_1357() {
+        assertTrue(new LinearIn().linearIn(
+                new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9},
+                new int[]{1, 3, 5, 7}));
+    }
+}
